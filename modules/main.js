a4d3692095982229f68291b125b67cd88d0ef0e1
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-10-21 16:48:11 +0300

    Update main.js

--- modules/main.js
+++ modules/main.js
@@ -1,40 +1,27 @@
 import LocalStorageHandler from "./local.js"
 import Product from "./product.js"
-
 const productsStorageHandler = new LocalStorageHandler("products")
-
 const addProduct = document.querySelector(".add-product")
 const enterProduct = document.querySelector(".enter-product")
 const enterPrice = document.querySelector(".enter-price")
 const btn = document.querySelector(".add")
 const total = document.querySelector(".total")
 const noItem = document.querySelector(".no-item")
-
-
-
 const products = productsStorageHandler.getProductsFromLocalStorage()
-/* if(productsStorageHandler.getProductsFromLocalStorage()){
+
+
+if(productsStorageHandler.getProductsFromLocalStorage()){
     products.map(product => {
         Addproduct(product)
-        console.log(product)
-        
     })
-} */
-
+}
 const createNewProduct = (title, price, id) => {
     const newProduct = new Product(title, price, id)
-    console.log(newProduct)
-
+    console.log(newProduct.title)
     products.push(newProduct)
-    products.map(product => {
-        Addproduct(product)
-        
-        
-    })
     console.log(products)
     productsStorageHandler.addProductToLocalStorage(newProduct)
-    /* Addproduct(newProduct)
-    addition(price) */
+    Addproduct(newProduct)
 }
 btn.addEventListener("click", () => {
     createNewProduct(enterProduct.value, enterPrice.value, Date.now())
@@ -56,35 +43,12 @@ function Addproduct(product) {
     div.appendChild(p2)
     div.appendChild(p3)
     addProduct.appendChild(div)
-
-
-
-
 }
 addProduct.addEventListener("click", (e) => {
     const hasclass = e.target.classList.contains("x")
     if (hasclass) {
         productsStorageHandler.deleteProductFromLocalStorage(e.target.parentElement.getAttribute("id")) 
         e.target.parentElement.remove();
-/*         noItem.style.display = "block"
- */       
-       
-
+      
     }
-
-
-    
-
 })
-
-function addition(price) {
-    price=0
-    const count = [price]
-    count.forEach(price => {
-        price += price
-        total.innerHTML = `Total Price:${price}`
-
-    })
-
-
-}
